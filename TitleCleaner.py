951190733cc3ceff0409541bec200043f446c38d
jclane <justin@justinlane.me>
2019-07-18 19:35:14 +0000

    Added additional error checking and help text to argparse.

--- TitleCleaner.py
+++ TitleCleaner.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3.7
+#!/usr/bin/env python3
 
 import argparse
 from os import walk as oswalk
 from os import makedirs as osmakedirs
 from os.path import join as ospathjoin
 from os.path import isdir as osisdir
+from os.path import isfile as osisfile
 from os.path import splitext as ossplitext
 from os.path import exists as osexists
 from os.path import dirname as osdirname
@@ -56,29 +57,34 @@ def process_filenames(files, vid_type, output_dir):
 
 def check_type(type_arg):
     if type_arg.lower() in ("movie", "series"):
-        if type_arg.lower() == "movie":
-            return "Movie"
-        else:
-            return "Series"
+        return "Movie" if type_arg.lower() == "movie" else "Series"
+    else:
+        raise argparse.ArgumentTypeError("Type should be either 'movie' or 'series'.")
+
 
 def check_input_path(input_arg):
     if osexists(input_arg):
         return input_arg
     else:
-        raise argparse.ArgumentTypeError("Supplied path is not valid.  No such file or directory.")
+        raise argparse.ArgumentTypeError("Supplied input path is not valid.  No such file or directory.\n" + input_arg)
 
 parser = argparse.ArgumentParser(prog="TitleCleaner",
                                  description='Clean up torrented video file names.')
 parser.add_argument("-r", "-R", "--recursive", dest="recursive", action="store_true",
                     help="clean video file names recursively")
-parser.add_argument("vid_type", type=check_type, help="type of video ('Movie' or 'Series')")
-parser.add_argument("INPUT", type=check_input_path, help="path to file or folder to clean")
-parser.add_argument("OUTPUT", help="path to folder to save cleaned files")
+parser.add_argument("-t, --type", dest="vid_type", type=check_type, help="type of video ('Movie' or 'Series')")
+parser.add_argument("input_path", type=check_input_path, help="path to file or folder to clean")
+parser.add_argument("output_path", type=str, help="path to folder to save cleaned files")
 args = parser.parse_args()
 
 if args.recursive:
-    files = get_filenames(args.INPUT)
-    process_filenames(files, args.vid_type, args.OUTPUT)
+    if osisdir(args.input_path):
+        files = get_filenames(args.input_path)
+        #process_filenames(files, args.vid_type, args.output_path)
+    else:
+        parser.error("The rescursive flag is set, but the supplied input points to a file.")
 else:
-    process_filenames([args.INPUT], args.vid_type, args.OUTPUT)
-
+    if osisfile(args.input_path) and ossplitext(args.input_path)[1] in (".mkv", ".mp4"):
+        process_filenames([args.input_path], args.vid_type, args.output_path)
+    else:
+        parser.error("If recursive flag is set then the input path must point to a video file.")
